48a6f4ff4fece4f0aa5b9602016dc7a423f6ed19
Gilles Chardon <gilles.chardon.fr@gmail.com>
2022-09-11 14:51:24 +0900

    update tests and minor updates on .env.example and scrap_blog_articles.py

--- scrappackage/scrap_blog_articles.py
+++ scrappackage/scrap_blog_articles.py
@@ -12,45 +12,52 @@ from datetime import datetime
 
 class Website_blog:
     """
-    A class for scrap a particular website's blog articles
-    The environment variable BLOG_URL contains the website URL
+    A class for scrap a particular website's blog articles.
+    The environment variable BLOG_URL contains the website URL.
+    I use an environement variable to prevent malicious use of my script.
     """
 
     def sleep_random_time(self):
         """
-        sleep method to avoid the website server to be overloaded
-        with too many requests in a short time
+        Sleep method to avoid the website server to be overloaded
+        with too many requests in a short time.
         :return: None
         :rtype: NoneType
         """
         # Program execution will be stopped for a period of time
         # ranging from 5 to 16 seconds.
-        wait_time = random.randint(1, 5)
+        wait_time = random.randint(5, 25)
         time.sleep(wait_time)
 
 
     def get_website_blog_url(self):
         """
-        get the the absolute path of the root directory
+        Get the the absolute path of the root directory
         and read the .env file to get the environement variable
-        which contains the website's blog url
-        .env file must contain only one environment variable
+        which contains the website's blog url.
+        '.env' file must contain only one environment variable.
         :return: website's blog url
         :rtype: string
         """
-        # get and return the absolute path of the root directory
+        # Get and return the absolute path of the root directory.
         root_abs_path = os.path.dirname(
             os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
             )
-        with open(os.path.join(root_abs_path, ".env")) as f:
-            _, text = f
 
-        return text.split("=")[1].strip()
+        # get the environment variables
+        with open(os.path.join(root_abs_path, ".env")) as f:
+            env_var_dict = {}
+            for line in f:
+                var_name, value = line.split("=")
+                env_var_dict[var_name] = value
+
+        # get and return the website URL
+        return env_var_dict["WEBSITE_BLOG_URL_TEST"].strip()
 
 
     def convert_date(self, date: str = "") -> str:
         """
-        Convert a date to "YYYY-MM-DD" format
+        Convert a date to "YYYY-MM-DD" format.
         Example: "June 17th, 2020" returns "2020-06-17"
         :param date: date with "month DDth, YYYY" format
         :type date: str
@@ -71,7 +78,7 @@ class Website_blog:
             except Exception as e:
                 raise e
         raise ValueError(
-            "Date doesn't have the required format. For example:\n"
+            "Date doesn't have the required format. Examples of correct date format:\n"
             "February 1st, 2017'  'May 2nd, 1997'  'June 3rd, 2008'  'March 4th, 2015'"
             )
 
@@ -87,28 +94,28 @@ class Website_blog:
         :rtype: dict
         """
 
-        # setup the request and soup setup
+        # Setup the request and soup setup.
         data = requests.get(url)
         soup = BeautifulSoup(data.text, "html.parser")
 
-        # articles' page can have different layout
-        # so let's use try/except to avoid interupting the script
+        # Articles' page can maybe have different layout
+        # so let's use try/except to avoid interupting the script.
 
-        #article's date
+        # Article's date
         try:
             date = soup.find("div", "hero__meta") \
                     .span.string.split("|")[0].strip()
         except:
             date=""
 
-        #article's author
+        # Article's author
         try:
             author = soup.find("div", "hero__meta") \
                     .span.string.split("|")[1].strip()
         except:
             author=""
 
-        # list of article's tags
+        # List of article's tags
         try:
             tags = ", ".join(
                 [tag.a.string.strip() for tag in soup.find_all("li", "tags__tag")]
@@ -116,7 +123,7 @@ class Website_blog:
         except:
             tags = ""
 
-        # list of article's' leads and text
+        # List of article's' leads and text
         try:
             text = " ".join(
                     [p.get_text().strip().replace("\n", " ") for p in soup \
@@ -144,24 +151,24 @@ class Website_blog:
         :rtype: pandas.core.frame.DataFrame
         """
 
-        # setup the url, request, soup setup and scrap.
+        # Setup the url, request, soup setup and scrap.
         page_url = f"{self.get_website_blog_url()}1/"
         data = requests.get(page_url)
         soup = BeautifulSoup(data.text, "html.parser")
 
-        # display the page URL
+        # Display the page URL
         print(f"Scrap articles from URL page: {page_url}")
 
-        # top article title
+        # Top article title
         top_title = soup.find("h1", "hero__title").a.string.strip()
 
-        # top article url
+        # Top article url
         top_url = soup.find("h1", "hero__title").a["href"]
 
-        # top article categories
-        # we need to go on the article page, and get the categories
-        # "home" is excluded as it is just a link to the home page
-        # categories are stored as a string
+        # Top article categories.
+        # We need to go on the article page, and get the categories.
+        # "home" is excluded as it is just a link to the home page.
+        # Categories are stored as a string
         data_top_article = requests.get(top_url)
         soup_top_article = BeautifulSoup(data_top_article.text, "html.parser")
         top_categories = ", ".join(
@@ -169,15 +176,15 @@ class Website_blog:
                 .find_all("a") if cat.string != "Home"]
             )
 
-        # list of titles and urls
-        # for the top picks featured articles only (the two following articles)
+        # List of titles and urls
+        # for the top picks featured articles only (the two following articles).
         titles_soup = soup.find_all("h3", "card__title")
         pick_titles = [titles_soup[i].span.string.strip() for i in range(2)]
         url_soup = soup.find_all("div", "card__image")
         picks_urls = [url_soup[i].a["href"] for i in range(2)]
 
-        # list of titles and urls
-        # for the other articles (15)
+        # List of titles and urls
+        # for the other articles (15).
         other_titles = []
         other_urls = []
         other_titles_soup = soup.find_all("ul", "grid")
@@ -188,7 +195,7 @@ class Website_blog:
         other_titles = sum(other_titles, [])
         other_urls = sum(other_urls, [])
 
-        # categories of articles except the top one:
+        # Categories of articles except the top one:
         # 1- soup setup for categories scrapping
         # 2- for each article, store a string of categories
         # (for 2+15=17 articles)
@@ -197,20 +204,20 @@ class Website_blog:
             [category.string for category in categories_soup[i].find_all("a")]
             ) for i in range(17)]
 
-        # lists of titles, urls and categories for all the articles
-        # of the first page
+        # Lists of titles, urls and categories for all the articles
+        # of the first page.
         df_titles = pd.DataFrame(
             {"title": [top_title] + pick_titles + other_titles})
         df_urls = pd.DataFrame({"url": [top_url] + picks_urls + other_urls})
         df_categories = pd.DataFrame(
             {"categories": [top_categories] + other_articles_categories})
 
-        # create:
-        # a list of articles' date,
-        # a list of articles' author,
-        # a list of articles' tags,
-        # a list of articles' leads
-        # for each articles: scrap data and append those 4 lists
+        # Create:
+        # - a list of articles' date,
+        # - a list of articles' author,
+        # - a list of articles' tags,
+        # - a list of articles' leads.
+        # For each articles: scrap data and append those 4 lists.
         date_for_each_article = []
         author_for_each_article = []
         tags_for_each_article = []
@@ -225,18 +232,18 @@ class Website_blog:
             print("*", end="", flush=True) # simple progression bar
             self.sleep_random_time()
 
-        # display the number of articles scrapped right after
-        # the simple progression bar
+        # Display the number of articles scrapped right after
+        # the simple progression bar.
         print(f" {len(df_urls)} articles scrapped on this page.\n")
 
-        # create pandas dataframe for articles' date,
-        # articles' author, articles' tags, and articles' text
+        # Create pandas dataframe for articles' date,
+        # articles' author, articles' tags, and articles' text.
         df_dates = pd.DataFrame({'date': date_for_each_article})
         df_authors = pd.DataFrame({'author': author_for_each_article})
         df_tags = pd.DataFrame({'tags': tags_for_each_article})
         df_texts = pd.DataFrame({'text': text_for_each_article})
 
-        # concatenate the previous df and return the final df
+        # Concatenate the previous df and return the final df
         return pd.concat(
             [df_urls, df_titles, df_dates, df_authors, df_categories,
              df_tags, df_texts], axis=1
@@ -256,14 +263,14 @@ class Website_blog:
         :rtype: pandas.core.frame.DataFrame
         """
 
-        # check if the argument is valid (int >= 2)
+        # Check if the argument is valid (int >= 2)
         try:
             if int(page) <= 1:
                 raise ValueError("argument must be integer >= 2")
         except ValueError:
             raise ValueError("argument must be integer >= 2")
 
-        # setup the url, request, soup setup and scrap.
+        # Setup the url, request, soup setup and scrap.
         # Raise exception if page not found
         page_url = f"{self.get_website_blog_url()}{int(page)}/"
         data = requests.get(page_url)
@@ -271,11 +278,11 @@ class Website_blog:
         if soup.h1.string == 'Page not found':
             raise ConnectionError('Page not found')
 
-        # display the page URL
+        # Display the page URL
         print(f"Scrap articles from URL page: {page_url}")
 
-        # create pandas dataframes for all the titles
-        # and all the articles' urls
+        # Create pandas dataframes for all the titles
+        # and all the articles' urls.
         titles_soup = soup.find_all("h3", "card__title")
         df_titles = pd.DataFrame(
             {'title': [title.a["title"] for title in titles_soup]}
@@ -284,21 +291,21 @@ class Website_blog:
             {'url': [title.a["href"] for title in titles_soup]}
             )
 
-        # create a pandas dataframe to store the categories of all the articles of the page:
-        # 1- soup setup for categories scrapping
-        # 2- for each article, store a string of categories
+        # Create a pandas dataframe to store the categories of all the articles of the page:
+        # 1- soup setup for categories scrapping,
+        # 2- for each article, store a string of categories.
         categories_soup = soup.find_all("div", "card__category label")
         categories_for_each_article = [", ".join(
             [category.string for category in categories.find_all("a")]
             ) for categories in categories_soup]
         df_categories = pd.DataFrame({'categories': categories_for_each_article})
 
-        # create:
-        # a list of articles' date,
-        # a list of articles' author,
-        # a list of articles' tags,
-        # a list of articles' leads
-        # for each articles: scrap data and append those 4 lists
+        # Create:
+        # - a list of articles' date,
+        # - a list of articles' author,
+        # - a list of articles' tags,
+        # - a list of articles' leads
+        # For each articles: scrap data and append those 4 lists.
         date_for_each_article = []
         author_for_each_article = []
         tags_for_each_article = []
@@ -313,18 +320,18 @@ class Website_blog:
             print("*", end="", flush=True) # simple progression bar
             self.sleep_random_time()
 
-        # display the number of articles scrapped right after
-        # the simple progression bar
+        # Display the number of articles scrapped right after
+        # the simple progression bar.
         print(f" {len(df_urls)} articles scrapped on this page.\n")
 
-        # create pandas dataframe for articles' date,
+        # Create pandas dataframe for articles' date,
         # articles' author, articles' tags, and articles' text
         df_dates = pd.DataFrame({'date': date_for_each_article})
         df_authors = pd.DataFrame({'author': author_for_each_article})
         df_tags = pd.DataFrame({'tags': tags_for_each_article})
         df_texts = pd.DataFrame({'text': text_for_each_article})
 
-        # concatenate the previous df and return the final df
+        # Concatenate the previous df and return the final df
         return pd.concat(
             [df_urls, df_titles, df_dates, df_authors, df_categories,
              df_tags, df_texts], axis=1
@@ -339,7 +346,7 @@ class Website_blog:
         :rtype: pandas.core.frame.DataFrame
         """
 
-        # Scrap first articles' page
+        # Scrap first articles' page.
         df_all_articles = self.scrap_blog_page_one_only()
 
         # Scrap the following articles' pages and concat the df
@@ -355,7 +362,7 @@ class Website_blog:
             if len(df_page) == 0:
                 break
             df_all_articles = pd.concat([df_all_articles, df_page])
-            if page == 3:
+            if page >= 75:
                 break
             page += 1
         return df_all_articles
@@ -370,11 +377,6 @@ class Website_blog:
 
 def main():
     print("The library scrape_one_page.py has been ran directly.")
-    wb = Website_blog()
-    df = wb.scrap_all_blog_articles()
-    file_name = f"all_articles.csv"
-    df.to_csv(file_name, mode='w', index=None, header=True)
-    print("fini")
 
 
 if __name__ == "__main__":
--- tests/test_scrape_one_page.py
+++ tests/test_scrape_one_page.py
@@ -1,26 +1,88 @@
 """
 tests scrape_one_page.py
 """
+import pytest
 import os
-from scrappackage.scrap_blog_articles import Website_blog
+from scrappackage.scrap_blog_articles import Website_blog, main
 
 
 wb = Website_blog()
 
+def test_sleep_random_time():
+    assert wb.sleep_random_time() == None
+
+
+def test_get_website_blog_url():
+    # Get and return the absolute path of the root directory.
+    root_abs_path = os.path.dirname(
+    os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+    )
+    # read the .env file to get the url for testing purpose.
+    with open(os.path.join(root_abs_path, ".env")) as f:
+        env_var_dict = {}
+        for line in f:
+            var_name, value = line.split("=")
+            env_var_dict[var_name] = value
+    WEBSITE_BLOG_URL_TEST = env_var_dict["WEBSITE_BLOG_URL_TEST"].strip()
+    assert WEBSITE_BLOG_URL_TEST == wb.get_website_blog_url()
+
+
+def test_convert_date_correct_date_format():
+    assert wb.convert_date("June 17th, 2020") == "2020-06-17"
+
+
+def test_convert_date_correct_date_wrong_format_1():
+    with pytest.raises(Exception):
+        wb.convert_date("June 17th 2020")
+
+
+def test_convert_date_correct_date_wrong_format_2():
+    with pytest.raises(Exception):
+        wb.convert_date("August, 3rd 2021")
+
+
+def test_convert_date_correct_date_wrong_format_3():
+    with pytest.raises(Exception):
+        wb.convert_date("2019 September 3")
+
+
+def test_scrap_one_article_page():
+    # Get and return the absolute path of the root directory.
+    root_abs_path = os.path.dirname(
+    os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+    )
+    # read the .env file to get an article's url for testing purpose.
+    with open(os.path.join(root_abs_path, ".env")) as f:
+        env_var_dict = {}
+        for line in f:
+            var_name, value = line.split("=")
+            env_var_dict[var_name] = value
+    WEBSITE_ARTICLE_PAGE_TEST = env_var_dict["WEBSITE_ARTICLE_PAGE_TEST"].strip()
+    returned_data = wb.scrap_one_article_page(WEBSITE_ARTICLE_PAGE_TEST)
+    assert len(returned_data) == 4
+
 
 def test_scrap_blog_page_one_only():
-    db = wb.scrap_blog_page_one_only()
-    file_name = f"websiteblog_first_page.csv"
-    db.to_csv(file_name, mode='w', index=None, header=True)
-    assert os.path.exists(file_name)
+    df = wb.scrap_blog_page_one_only()
+    assert len(df) == 18
+    assert len(df.columns) == 7
 
 
 def test_scrap_blog_one_page_for_page_two_and_more():
     page = 8
-    db = wb.scrap_blog_one_page_for_page_two_and_more(page)
-    file_name = f"websiteblog_onepage_page_{page}.csv"
-    db.to_csv(file_name, mode='w', index=None, header=True)
-    assert os.path.exists(file_name)
+    df = wb.scrap_blog_one_page_for_page_two_and_more(page)
+    assert len(df) == 24
+    assert len(df.columns) == 7
 
+"""
+def test_scrap_all_blog_articles():
+    df = wb.scrap_all_blog_articles()
+    file_name = f"all_articles.csv"
+    df.to_csv(file_name, mode='w', index=None, header=True)
+"""
 
-# other tests to write
+def test_ping():
+    assert wb.ping() == None
+
+def test_main():
+    assert main() == None
